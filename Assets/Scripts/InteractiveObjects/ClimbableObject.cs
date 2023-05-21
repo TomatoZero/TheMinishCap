358a62e6fe0ff8d451a592d3cbdf56600dacf106
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-21 18:56:09 +0300

    feat(game logic): implement the script of the object for climbing on the floor

---
+++ Assets/Scripts/InteractiveObjects/ClimbableObject.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbableObject : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out CharacterController controller))
+        {
+            controller.ClimbingStart();
+            // controller.MoveTo(this.transform.position.x);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out CharacterController controller))
+        {
+            controller.ClimbingEnd();
+        }
+    }
+}
