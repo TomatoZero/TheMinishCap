50414ac35c47bc216e4020536ceb8d7272cee002
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-21 19:46:18 +0300

    feat(game logic): implement HoveringStart method (Assets/Scripts/Link/CharacterController)

--- Assets/Scripts/Link/CharacterController.cs
+++ Assets/Scripts/Link/CharacterController.cs
@@ -9,13 +9,19 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _rowSpeedMultiplier = 5f;
     [SerializeField] private float _climbSpeedMultiplier;
+    [SerializeField] private float _hoveringSpeedMultiplier;
     [SerializeField] private float _rollTime = 0.5f;
+    [SerializeField] private float _hoveringTime = 1f;
+    [SerializeField] private AnimationCurve _curveHovering;
     
     private Rigidbody2D _rigidbody;
-    private BoxCollider2D _collider;
     private Vector2 _moveDirection;
     private Vector2 _rollDirection;
     private float _currentSpeed;
+    private Vector2 _currentPos;
+    private Vector2 _landingPos;
+    private float _landingDistance;
+    private float _timeElapsed;
     private List<SpriteRenderer> _childSprite;
     private State _currentState = State.Ground;
 
@@ -37,7 +43,6 @@ public class CharacterController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = this.GetComponent<Rigidbody2D>();
-        _collider = this.GetComponent<BoxCollider2D>();
         _currentSpeed = _moveSpeed;
 
         _childSprite = this.GetComponents<SpriteRenderer>().ToList();
@@ -45,7 +50,25 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rigidbody.MovePosition(_rigidbody.position + MoveDirection * (_currentSpeed * Time.fixedDeltaTime));
+        if(_currentState != State.Hovering)
+        {
+            _rigidbody.MovePosition(_rigidbody.position + MoveDirection * (_currentSpeed * Time.fixedDeltaTime));
+        }
+        else if (_currentState == State.Hovering)
+        {
+            _timeElapsed += Time.fixedDeltaTime * _currentSpeed / _landingDistance;
+            if (_timeElapsed < _hoveringTime)
+            {
+                _currentPos = Vector2.MoveTowards(_currentPos, _landingPos, Time.fixedDeltaTime * _currentSpeed);
+                _rigidbody.MovePosition(new Vector2(_currentPos.x,
+                    _currentPos.y + _curveHovering.Evaluate(_timeElapsed)));
+            }
+            else
+            {
+                _currentState = State.Ground;
+            }
+        }
+            
     }
 
     public void ChangeLayer(string layer)
@@ -85,6 +108,16 @@ public class CharacterController : MonoBehaviour
         _currentState = State.Ground;
     }
 
+    public void HoveringStart()
+    {
+        _currentPos = _rigidbody.position;
+        _currentSpeed = _moveSpeed * _rowSpeedMultiplier;
+        _landingPos = _currentPos + _moveDirection.normalized * _currentSpeed;
+        _landingDistance = Vector2.Distance(_landingPos, _currentPos);
+        _timeElapsed = 0f;
+        _currentState = State.Hovering;
+    }
+    
     public void MoveTo(float x)
     {
         _rigidbody.position = new Vector2(x, _rigidbody.position.y);
@@ -101,6 +134,7 @@ public class CharacterController : MonoBehaviour
     {
         Ground,
         Ladder,
-        Roll
+        Roll,
+        Hovering
     }
 }
