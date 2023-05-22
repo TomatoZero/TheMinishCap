d945066065dbbcb404ccd6f83e47020319a7fe29
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-22 22:37:41 +0300

    feat(ScriptableObject): add read-only property

--- Assets/Scripts/ScriptableObjects/KinstonePiece/KinstonePieceData.cs
+++ Assets/Scripts/ScriptableObjects/KinstonePiece/KinstonePieceData.cs
@@ -9,4 +9,11 @@ public class KinstonePieceData : ScriptableObject
     [SerializeField] private KinstonePieceType _type;
     [SerializeField] private int _class;
     [SerializeField] private int _number;
+
+    public string Name => _name;
+    public Sprite Icon => _icon;
+    public KinstonePieceType Type => _type;
+    public int Class => _class; 
+    public int Number => _number;
+    
 }
--- Assets/Scripts/ScriptableObjects/TigerScroll/TigerScrollData.cs
+++ Assets/Scripts/ScriptableObjects/TigerScroll/TigerScrollData.cs
@@ -6,4 +6,8 @@ public class TigerScrollData : ScriptableObject
     [SerializeField] private string _name;
     [SerializeField] private Sprite _icon;
     [SerializeField] private Sprite _description;
+
+    public string Name => _name;
+    public Sprite Icon => _icon;
+    public Sprite Description => _description;
 }
