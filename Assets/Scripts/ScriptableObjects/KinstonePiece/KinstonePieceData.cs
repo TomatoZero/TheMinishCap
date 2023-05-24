2b64b8291bbb8d675fbbd27468efc831581fa4cc
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-24 10:36:00 +0300

    feat(scriptableObject): add value count in bag for KinstonePiece

--- Assets/Scripts/ScriptableObjects/KinstonePiece/KinstonePieceData.cs
+++ Assets/Scripts/ScriptableObjects/KinstonePiece/KinstonePieceData.cs
@@ -9,11 +9,12 @@ public class KinstonePieceData : ScriptableObject
     [SerializeField] private KinstonePieceType _type;
     [SerializeField] private int _class;
     [SerializeField] private int _number;
-
+    [SerializeField] private int _countInBag;
+    
     public string Name => _name;
     public Sprite Icon => _icon;
     public KinstonePieceType Type => _type;
     public int Class => _class; 
     public int Number => _number;
-    
+    public int CountInBag => _countInBag;
 }
