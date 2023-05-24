2fa4d3a02d0ddc0f2ace07dfc4f805094117ba29
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-24 10:47:51 +0300

    feat(script): add InventoryContoller for character

--- Assets/Scripts/Link/CharacterController.cs
+++ Assets/Scripts/Link/CharacterController.cs
@@ -11,17 +11,12 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private float _climbSpeedMultiplier;
     [SerializeField] private float _hoveringSpeedMultiplier;
     [SerializeField] private float _rollTime = 0.5f;
-    [SerializeField] private float _hoveringTime = 1f;
-    [SerializeField] private AnimationCurve _curveHovering;
-    
+
     private Rigidbody2D _rigidbody;
     private Vector2 _moveDirection;
     private Vector2 _rollDirection;
     private float _currentSpeed;
-    private Vector2 _currentPos;
-    private Vector2 _landingPos;
-    private float _landingDistance;
-    private float _timeElapsed;
+
     private List<SpriteRenderer> _childSprite;
     private State _currentState = State.Ground;
 
@@ -50,25 +45,7 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(_currentState != State.Hovering)
-        {
-            _rigidbody.MovePosition(_rigidbody.position + MoveDirection * (_currentSpeed * Time.fixedDeltaTime));
-        }
-        else if (_currentState == State.Hovering)
-        {
-            _timeElapsed += Time.fixedDeltaTime * _currentSpeed / _landingDistance;
-            if (_timeElapsed < _hoveringTime)
-            {
-                _currentPos = Vector2.MoveTowards(_currentPos, _landingPos, Time.fixedDeltaTime * _currentSpeed);
-                _rigidbody.MovePosition(new Vector2(_currentPos.x,
-                    _currentPos.y + _curveHovering.Evaluate(_timeElapsed)));
-            }
-            else
-            {
-                _currentState = State.Ground;
-            }
-        }
-            
+        _rigidbody.MovePosition(_rigidbody.position + MoveDirection * (_currentSpeed * Time.fixedDeltaTime));
     }
 
     public void ChangeLayer(string layer)
@@ -108,21 +85,11 @@ public class CharacterController : MonoBehaviour
         _currentState = State.Ground;
     }
 
-    public void HoveringStart()
+    public void JumpFromEdge()
     {
-        _currentPos = _rigidbody.position;
-        _currentSpeed = _moveSpeed * _rowSpeedMultiplier;
-        _landingPos = _currentPos + _moveDirection.normalized * _currentSpeed;
-        _landingDistance = Vector2.Distance(_landingPos, _currentPos);
-        _timeElapsed = 0f;
-        _currentState = State.Hovering;
+        
     }
-    
-    public void MoveTo(float x)
-    {
-        _rigidbody.position = new Vector2(x, _rigidbody.position.y);
-    }
-    
+
     private IEnumerator StopRolling()
     {
         yield return new WaitForSeconds(_rollTime);
@@ -134,7 +101,6 @@ public class CharacterController : MonoBehaviour
     {
         Ground,
         Ladder,
-        Roll,
-        Hovering
+        Roll
     }
 }
---
+++ Assets/Scripts/Link/InventoryController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    [SerializeField] private InventoryData _inventory;
+
+    private void Awake()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<WeaponItem>(out var weapon))
+        {
+            _inventory.AddWeapon(weapon.Data);
+        }
+    }
+}
