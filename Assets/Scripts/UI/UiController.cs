56758dbfa2da1feffc0ccd8e71fd978c43ce37a8
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-05-29 12:41:07 +0300

    feat(sprites): add images of controller buttons

--- Assets/Scripts/UI/UiController.cs
+++ Assets/Scripts/UI/UiController.cs
@@ -1,7 +1,7 @@
-using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UiContoller : MonoBehaviour
 {
@@ -10,17 +10,26 @@ public class UiContoller : MonoBehaviour
     [SerializeField] private MapPanelController _mapPanel;
     [Space]
     [SerializeField] private DungeonMapPanelController _dungeonMapPanel;
-
-    [Space] [SerializeField] private TMP_Text _currenWindowName;
+    [Space]
+    [SerializeField] private TMP_Text _currenWindowName;
     [SerializeField] private DescriptionController _description;
     [SerializeField] private List<WindowController> _windows;
-
+    [SerializeField] private List<ButtonHintController> _buttonsHint;
+    
     private int _currentWindow;
     
     public string WindowName
     {
         get => _currenWindowName.text;
         set => _currenWindowName.text = value;
+    }
+
+    private void Awake()
+    {
+        InputSystem.onDeviceChange += (device, change) =>
+        {
+           Debug.Log($"{device} {change}");
+        };
     }
 
     private void OnEnable()
@@ -51,7 +60,6 @@ public class UiContoller : MonoBehaviour
             WindowName = _windows[0].WindowName;
             _windows[0].Show();
         }
-        
     }
     
     public void PrevWindow()
@@ -71,15 +79,46 @@ public class UiContoller : MonoBehaviour
             _windows[^1].Show();
         }
     }
-
+    
     public void SetDescription(string description)
     {
         _description.SetDescription(description);
     }
-    
 
     public void HideDescription()
     {
         _description.Close();
+    }
+
+    public void SetFirstButtonHint(IventoryItem item)
+    {
+        SetButtonHint(0, item.Item.Icon);
+    }
+    
+    public void SetSecondButtonHint(IventoryItem item)
+    {
+        SetButtonHint(1, item.Item.Icon);
+    }
+
+    public void SetFirstButtonHint(string hint)
+    {
+        SetButtonHint(0, hint);
+    }
+
+    public void HideButtonHint(int id)
+    {
+        _buttonsHint[id].Hide();
+    }
+
+    private void SetButtonHint(int id, Sprite itemIco)
+    {
+        _buttonsHint[id].Show();
+        _buttonsHint[id].ItemIco = itemIco;
+    }
+
+    private void SetButtonHint(int id, string hint)
+    {
+        _buttonsHint[id].Show();
+        _buttonsHint[id].TextHint = hint;
     }
 }
